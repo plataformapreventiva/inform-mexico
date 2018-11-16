62b37f3a1acf91b0e791172c079e1ba0a2d9c0dd
Roberto Sánchez Ávalos <r.sanchezavalos@gmail.com>
2018-11-16 20:34:14 +0000

    typo test on model

--- models/inform-index.R
+++ models/inform-index.R
@@ -78,7 +78,7 @@ if(length(opt) > 1){
 
   employee <- c('John Doe','Peter Gynn','Jolie Hope')
   salary <- c(21000, 23400, 26800)
-  startdate <- as.Date(c('2010-11-1','2008-3-25','2007-3-14')
+  startdate  <- c(21000, 23400, 26800)
   db <- data.frame(employee, salary, startdate)
   copy_to(con, db,
           dbplyr::in_schema("models",opt$pipeline),
