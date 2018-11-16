60bacdfb55f348f9b2b26ef567570eefa817ad41
Roberto Sánchez Ávalos <r.sanchezavalos@gmail.com>
2018-11-16 16:46:59 +0000

    Added handler

---
+++ models/inform-index.R
@@ -0,0 +1,90 @@
+#!/usr/bin/env Rscript
+#!/usr/bin/env Rscript
+library(optparse)
+library(dbplyr)
+library(dplyr)
+library(DBI)
+library(lubridate)
+library(yaml)
+
+option_list = list(
+  make_option(c("--current_date"), type="character", default="",
+              help="current date", metavar="character"),
+  make_option(c("--database"), type="character", default="",
+              help="database name", metavar="character"),
+  make_option(c("--user"), type="character", default="",
+              help="database user", metavar="character"),
+  make_option(c("--password"), type="character", default="",
+              help="password for datbase user", metavar="character"),
+  make_option(c("--host"), type="character", default="",
+              help="database host name", metavar="character"),
+  make_option(c("--pipeline"), type="character", default="",
+              help="model pipeline", metavar="character")
+
+)
+
+opt_parser <- OptionParser(option_list=option_list)
+
+opt <- tryCatch(
+  {
+    parse_args(opt_parser)
+  },
+  error=function(cond) {
+    message("Error: Provide database connection arguments appropriately.")
+    message(cond)
+    print_help(opt_parser)
+    return(NA)
+  },
+  warning=function(cond) {
+    message("Warning:")
+    message(cond)
+    return(NULL)
+  },
+  finally={
+    message("Finished attempting to parse arguments.")
+  }
+)
+
+if(length(opt) > 1){
+
+  if (opt$database=="" | opt$user=="" |
+      opt$password=="" | opt$host=="" ){
+    print_help(opt_parser)
+    stop("Database connection arguments are not supplied.n", call.=FALSE)
+  }else{
+    PGDATABASE <- opt$database
+    POSTGRES_PASSWORD <- opt$password
+    POSTGRES_USER <- opt$user
+    PGHOST <- opt$host
+    PGPORT <- "5432"
+  }
+
+  if(opt$data_date == ""){
+  stop("Did not receive a valid data date, stopping", call.=FALSE)
+  }else{
+    data_date <- opt$data_date
+  }
+
+  con <- DBI::dbConnect(RPostgres::Postgres(),
+                        host = PGHOST,
+                        port = PGPORT,
+                        dbname = PGDATABASE,
+                        user = POSTGRES_USER,
+                        password = POSTGRES_PASSWORD
+  )
+
+
+  print('Pulling datasets')
+
+  employee <- c('John Doe','Peter Gynn','Jolie Hope')
+  salary <- c(21000, 23400, 26800)
+  startdate <- as.Date(c('2010-11-1','2008-3-25','2007-3-14')
+  db <- data.frame(employee, salary, startdate)
+  copy_to(con, db,
+          dbplyr::in_schema("models",opt$pipeline),
+          temporary = FALSE, overwrite = TRUE)
+
+  dbDisconnect(con)
+
+  print('Features written to: features.crimenes_tasas')
+}
