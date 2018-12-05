5776e0924f0553c27ebcbbcf5e89aeac2691965c
Roberto Sánchez Ávalos <r.sanchezavalos@gmail.com>
2018-12-05 03:55:04 +0000

    Add dependencies of model handler

--- models/inform_index/aux.R
+++ models/inform_index/aux.R
@@ -2,8 +2,51 @@
 
 # Dependencies
 
-paquetes <- c("ggplot2","tidyverse","plyr","scales",
-              "maptools","rgdal","ggmap")
+paquetes <- c("optparse", "dbplyr", "dplyr",
+              "DBI", "lubridate", "yaml",
+              "mice", "rlist", "car","psych",
+              "classInt")
 no_instalados <- paquetes[!(paquetes %in% installed.packages()[,"Package"])]
 if(length(no_instalados)) install.packages(no_instalados)
 lapply(paquetes, library, character.only = TRUE)
+
+
+get_cuts<-function(data, varx){
+  # Generación de Cortes
+
+  yy<-as.numeric(unlist(data[,varx] %>% drop_na() ))
+  yy<-yy[!is.infinite(yy)]
+  ## Se revisa orientación: ¿más es mejor o menos es mejor?
+  classes<-classIntervals(var=yy, n=5, style="kmeans") ## Se encuentran los puntos de corte
+  cutoffs<-classes$brks
+  yy<-as.numeric(unlist(data[,varx])) ## No se tiran na's (data ya imputado)
+  data[,varx] <<- cut(x=yy, include.lowest = T, breaks=cutoffs, labels=c(0,25,50,75,100))
+  #data[,paste(varx,"_kmeans", sep="")] <<- cut(x=yy, include.lowest = T, breaks=cutoffs, labels=c(0,25,50,75,100))
+  data[[eval(varx)]] <- as.numeric(as.character(data[[eval(varx)]]))
+}
+
+get_var_from_type <- function(estructura, pattern){
+  # Obtener variable del yaml con regex
+  variables <- names(list.search(estructura, .[grepl(pattern, .)], 'character'))
+  variables <- sapply(lapply(variables,
+                             str_match,
+                             pattern = "variable.(.*).tipo"),
+                      function(x) x[2])
+  return(variables)
+}
+
+get_var_from_name <- function(estructura, pattern){
+  # Obtener variable del yaml con regex
+  variables <- purrr::flatten(estructura) %>% unlist() %>% names() %>% list()
+  variables <- list.search(variables, .[grepl(pattern,.)])
+  variables <- lapply(variables,
+                      str_match,
+                      pattern = "variable.(.*).tipo")[[1]][,2]
+  return(unique(variables))
+}
+
+to_numeric <- function(data, variables){
+  data[variables] <- sapply(data[variables], as.character)
+  data[variables]<- sapply(data[variables], as.numeric)
+  return(data)
+}
--- models/inform_index/handler.R
+++ models/inform_index/handler.R
@@ -1,12 +1,5 @@
 #!/usr/bin/env Rscript
-library(optparse)
-library(dbplyr)
-library(dplyr)
-library(DBI)
-library(lubridate)
-library(yaml)
-library(mice)
-
+source("aux.R")
 option_list = list(
   make_option(c("--current_date"), type="character", default="",
               help="current date", metavar="character"),
@@ -33,7 +26,7 @@ opt <- tryCatch(
   },
   error=function(cond) {
     message("Error: Provide database connection arguments appropriately.")
-    message(cond) 
+    message(cond)
     print_help(opt_parser)
     return(NA)
   },
@@ -71,83 +64,83 @@ if(length(opt) > 1){
 
 
   print('Pulling datasets')
-  
+
   data <- tbl(con, dbplyr::in_schema('features','inform_variables_municipios')) %>% retrieve_result() %>%
     select(-c(actualizacion_sedesol,data_date,media_salario_m,media_salario_h,pobreza_porcentaje,inf_publ,indice_dif))
-  estructura <- read_yaml("data/estructura_indice.yaml")
-  
+  estructura <- read_yaml("../../data/estructura_indice.yaml")
+
   #----------------------------------------------------------------------------------------
-  
+
   # LIMPIEZA CATEGÓRICAS
-  
+
   # Categóricas -> Numéricas
   # Clasifica las categóricas por tipo
   # Como ya están categorizadas las volvemos numéricas
-  
+
   categoricas_5 <- get_var_from_type(estructura,"categorica_5")
-  data[,categoricas_5]  <- data[,categoricas_5] %>% 
-    mutate_all(funs(recode(var = ., 
+  data[,categoricas_5]  <- data[,categoricas_5] %>%
+    mutate_all(funs(car::recode(var = .,
                            recodes = "'Muy bajo'=0;
                            'Bajo'=25;
                            'Medio'=50;'Alto'=75;
                            'Muy alto'=100")))
-  
+
   categoricas_tsunami <- get_var_from_type(estructura,"categorica_tsunami")
-  data[,categoricas_tsunami]  <- data[,categoricas_tsunami] %>% 
-    mutate_all(funs(recode(var = ., 
+  data[,categoricas_tsunami]  <- data[,categoricas_tsunami] %>%
+    mutate_all(funs(recode(var = .,
                            recodes = "'Sin peligro'=0;
                            'Muy alto'=100")))
-  
+
   #Existencia de unidad de protección civil
   #{0-NA|1-si|2-no|3-en proceso de integracion|8-info no disponible|9-no se sabe}
   categoricas_nd_spnn1 <- get_var_from_type(estructura,"categorica_nd_spnn1")
-  data[,categoricas_nd_spnn1]  <- data[,categoricas_nd_spnn1] %>% 
-  mutate_all(funs(recode(var = ., 
+  data[,categoricas_nd_spnn1]  <- data[,categoricas_nd_spnn1] %>%
+    mutate_all(funs(recode(var = .,
                          recodes = "0=0;
                         1=0;2=100;
                          3=25;8=50;
                          9=50")))
-  
+
   #Existencia de Atlas de Riesgo
   #Participacion ciudadana
   #{0-NA|1-si|2-no|8-info no disponible|9-no se sabe}
   categoricas_nd_nsnn4 <- get_var_from_type(estructura,"categorica_nd_nsnn4")
-  data[,categoricas_nd_nsnn4]  <- data[,categoricas_nd_nsnn4] %>% 
-    mutate_all(funs(recode(var = ., 
+  data[,categoricas_nd_nsnn4]  <- data[,categoricas_nd_nsnn4] %>%
+    mutate_all(funs(recode(var = .,
                            recodes = "0=0;
                          1=0;2=100;
                          8=50;9=50")))
-  
+
   # Gobernabilidad
   # Existencia de órganos de participación ciudadana
   #{0-NA|1-si}
   categoricas_nd_nsnn1 <- get_var_from_type(estructura,"categorica_nd_nsnn1")
-  data[,categoricas_nd_nsnn1]  <- data[,categoricas_nd_nsnn1] %>% 
-    mutate_all(funs(recode(var = ., 
+  data[,categoricas_nd_nsnn1]  <- data[,categoricas_nd_nsnn1] %>%
+    mutate_all(funs(recode(var = .,
                            recodes = "0=100;1=0")))
-  
+
   # Cobro predial
   # {0-NA|1-gobierno municipal|2-gobierno de la Entidad Federativa|3-No se cobra|9-No se sabe}
   categoricas_aut_cobr <- get_var_from_type(estructura,"categorica_aut_cobr")
-  data[,categoricas_aut_cobr]  <- data[,categoricas_aut_cobr] %>% 
-    mutate_all(funs(recode(var = ., 
+  data[,categoricas_aut_cobr]  <- data[,categoricas_aut_cobr] %>%
+    mutate_all(funs(recode(var = .,
                            recodes = "0=0;
                         1=0;2=25;3=
                        3=100;9=50")))
-  
+
   # Mecanismos de transferencia
   # {(1+1+1)/3}
   categoricas_mec_tran <- get_var_from_type(estructura,"categorica_mec_tran")
-  data[,categoricas_mec_tran]  <- data[,categoricas_mec_tran] %>% 
-    mutate_all(funs(recode(var = ., 
+  data[,categoricas_mec_tran]  <- data[,categoricas_mec_tran] %>%
+    mutate_all(funs(recode(var = .,
                            recodes = "0=100;
                         1/3=75;2/3=50;1=0")))
-  
+
   # índice sist político estable
   # ranking 1-32
   categoricas_politico <- get_var_from_type(estructura,"categorica_politico")
-  data[,categoricas_politico]  <- data[,categoricas_politico] %>% 
-    mutate_all(funs(recode(var = ., 
+  data[,categoricas_politico]  <- data[,categoricas_politico] %>%
+    mutate_all(funs(recode(var = .,
                            recodes = "1=0;2=0;3=0;
                                       4=0;5=0;6=0;
                                       7=25;8=25;9=25;
@@ -159,112 +152,84 @@ if(length(opt) > 1){
                                       25=75;26=75;27=100;
                                       28=100;29=100;30=100;
                                          31=100;32=100")))
-  
+
   #----------------------------------------------------------------------------------------
-  
+
   # LIMPIEZA NUMÉRICAS
   # Define las variables numéricas (Obtener de yaml)
   numericas <- get_var_from_type(estructura,"^numerica$")
-  data[,numericas] <- lapply(data[, numericas], as.numeric) 
-  
+  data[,numericas] <- lapply(data[, numericas], as.numeric)
+
   # Queremos que inform siempre sea más es peor
   # Acá define las variables donde más sea mejor
   mas_mejor <- get_var_from_type(estructura,"^mas_mejor$")
-  data[,mas_mejor] <- data[,mas_mejor]*-1  
-  
+  data[,mas_mejor] <- data[,mas_mejor]*-1
+
   # Crear cortes
   # kmeans (por ahora)
   lapply(numericas, get_cuts, data=data)
-  
+
   amenazas <- get_var_from_name(estructura, "Amenazas")
   vulnerabilidad <- get_var_from_name(estructura, "Vulnerabilidad")
   capacidades <- get_var_from_name(estructura, "Capacidades")
-  data <- to_numeric(data, amenazas) 
+  data <- to_numeric(data, amenazas)
   data <- to_numeric(data, vulnerabilidad)
-  data <- to_numeric(data, capacidades) 
+  data <- to_numeric(data, capacidades)
 
   inform_input <- mice(data = data, m=5, method = "pmm", maxit = 10, seed = 500)
-  
-  i1 <- complete(inform_input,1)
+
+  i1 <- mice::complete(inform_input,1)
   #i2 <- complete(inform_input,2)
   #i3 <- complete(inform_input,3)
   #i4 <- complete(inform_input,4)
   #i5 <- complete(inform_input,5)
-  
+
   #----------------------------------------------------------------------------------------
-  
+
   # AGREGADO DE DIMENSIÓN
   dimensiones <- names(estructura$Inform$Dimensión)
   df <- NULL
-  
+  subdimension_l <- list()
+  subsubdimension_l <- list()
+
   # Media aritmética entre subdimensiones
   for (dimension in dimensiones) {
-    subdimensiones <- estructura$Inform$Dimensión[[eval(dimension)]] %>% 
+    subdimensiones <- estructura$Inform$Dimensión[[eval(dimension)]] %>%
       purrr::flatten() %>% names()
+    subdimension_l <- append(subdimension_l, subdimensiones)
     for (subdimension in subdimensiones) {
       Subsubdimensiones <- estructura$Inform$Dimensión[[
-        eval(dimension)]]$Subdimensión[[eval(subdimension)]] %>% 
+        eval(dimension)]]$Subdimensión[[eval(subdimension)]] %>%
         purrr::flatten() %>% names()
+      subsubdimension_l <- append(subsubdimension_l, Subsubdimensiones)
       for (Subsubdimensión in Subsubdimensiones) {
         variables <- get_var_from_name(estructura,Subsubdimensión)
         pca_subsub <- i1[variables] %>% prcomp()
         i1[Subsubdimensión] <- pca_subsub$x[,1] %>% as.data.frame()
-        
+
       }
       print(pca_subsub$sdev^2 / sum(pca_subsub$sdev^2))
-      i1[subdimension] <- i1[Subsubdimensiones] %>% as.data.frame() %>% 
+      i1[subdimension] <- i1[Subsubdimensiones] %>% as.data.frame() %>%
         rowMeans(na.rm = TRUE)
     }
-    i1[dimension] <- i1[subdimensiones] %>% as.data.frame() %>% 
+    i1[dimension] <- i1[subdimensiones] %>% as.data.frame() %>%
       rowMeans(na.rm = TRUE)
   }
-        
-  
+
+
   # Media geométrica entre dimensiones
   i1["INFORM"] <-apply(i1[dimensiones], 1,geometric.mean,na.rm=TRUE)
-  inform <- i1 %>% dplyr::select(cve_muni, INFORM,dimensiones,
+
+  inform <- i1 %>% dplyr::select(cve_muni, INFORM, dimensiones,
                                  amenazas,capacidades,vulnerabilidad,
-                                 `1. Amenazas y Exposición`,
-                                 `2. Falta de Capacidades de Respuesta`,
-                                 `3. Vulnerabilidad`,
-                                 `1.1 Fenómenos Naturales`,
-                                 `1.1.1 Fenómeno Hidrometeorológico`,
-                                 `1.1.2 Fenómeno Geológico`,
-                                 `1.2 Fenómenos Antropogénicos`,
-                                 `1.2.1 Violencia y Delincuencia`,                           
-                                 `1.2.2. Amenazas Demográficas`,
-                                 `2.1 Capacidad Institucional`, 
-                                 `2.1.1 Gestión de Riesgo de Desastre`,                      
-                                 `2.1.2 Acceso a la Justicia`,                                
-                                 `2.1.3 Reglamentación Básica`,                              
-                                 `2.1.4 Gobernabilidad`,
-                                 `2.2 Capacidad financiera`, 
-                                 `2.2.1 Finanzas Públicas`,                                   
-                                 `2.2.2 Capacidad Económica`,                                
-                                 `2.2.3 Administrativa / operativa`,  
-                                 `2.3 Capacidad de infraestructura`, 
-                                 `2.3.1 Comunicación y energía`,                              
-                                 `2.3.2 Sistema de Gestión Municipal`,                       
-                                 `2.3.3 Capacidad Instalada del Sistema de Salud`,            
-                                 `3.1 Vulnerabilidad Socioeconomica`, 
-                                 `3.1.1 Carencia por acceso a alimentación`,                 
-                                 `3.1.2 Rezago Educativo`,                                    
-                                 `3.1.3 Carencia por acceso a servicios de salud`,           
-                                 `3.1.4 Carencia por acceso a seguridad social`,              
-                                 `3.1.5 Carencia por calidad y espacios de vivienda`,        
-                                 `3.1.6 Carencia por acceso a servicios básicos de vivienda`, 
-                                 `3.1.7 Ingreso`,                                            
-                                 `3.2 Grupos Sociales en Condición de Vulnerabilidad`,
-                                 `3.2.1 Grupos etarios`,                                     
-                                 `3.2.2 Población indígena`,                                  
-                                 `3.2.3 Tipología de Hogares`,                               
-                                 `3.2.4 Desigualdad`,                                         
-                                 `3.2.5 Personas con discapacidad`)                          
+                                 unlist(subdimension_l),
+                                 unlist(subsubdimension_l))
+
   inform <- arrange(inform, desc(INFORM)) %>%
-    mutate(ranking = 1:nrow(inform)) 
-    
+    mutate(ranking = 1:nrow(inform))
+
   #----------------------------------------------------------------------------------------
-  
+
   # Save model table
   table_id = DBI::Id(schema = 'models', table = 'inform_index')
   copy_to(con, i1,
